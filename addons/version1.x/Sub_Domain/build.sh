028a21efee41e9c2cc98d9a453027f953e015f29
VidulHB <66768720+VidulHB@users.noreply.github.com>
2022-06-11 18:54:49 +0530

    yay test 1

--- addons/version1.x/Sub_Domain/build.sh
+++ addons/version1.x/Sub_Domain/build.sh
@@ -176,8 +176,8 @@ download_files() {
 print "Downloading files..."
 
 mkdir -p $PTERO/temp
-curl -sSLo $PTERO/temp/subdomain.tar.gz https://raw.githubusercontent.com/VidulHB/Pterodactyl-AutoAddons/blob/main/addons/version1.x/Sub_Domain/subdomain.tar.gz
-tar -xzvf $PTERO/temp/subdomain.tar.gz -C $PTERO/temp
+curl -sSLo $PTERO/temp/subdomain.tar https://raw.githubusercontent.com/VidulHB/Pterodactyl-AutoAddons/blob/main/addons/version1.x/Sub_Domain/subdomain.tar
+tar -xzvf $PTERO/temp/subdomain.tar -C $PTERO/temp
 cp -rf -- $PTERO/temp/subdomain/* "$PTERO"
 rm -rf $PTERO/temp
 }
